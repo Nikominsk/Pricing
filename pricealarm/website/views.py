487745c5c7a7cf41e758b666fce222c9471e6551
Nikominsk <nniko.geuenich@gmail.com>
2020-12-29 18:28:22 +0100

    advanced project setup

--- pricealarm/website/views.py
+++ pricealarm/website/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    if request.method == "POST":
+        email_input = request.POST['email-input']
+        return render(request, 'home.html', {'email_input': email_input})
+        
+    else:
+        return render(request, 'home.html', {})
+
+def impressum(request):
+    return render(request, 'impressum.html', {})
